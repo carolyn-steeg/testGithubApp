0ab43aac71f1b31fe9ed72493baf41456be69e8d
carolyn-steeg <23csteeg@pinewood.edu>
2021-02-24 13:41:27 -0800

    Initial Commit

---
+++ Shared/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  Shared
+//
+//  Created by Carolyn Steeg on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
